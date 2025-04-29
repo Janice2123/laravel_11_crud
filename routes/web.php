9755dff106be5e5929505a42fadca7038f0ab656
Janice Q. Casagan <janice.casagan@student.passerellesnumeriques.org>
2025-04-29 17:16:13 +0800

    I have added auth code for login and logout

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-Route::get('/', function () {
- return view('welcome');
-});
-Route::resource('products', ProductController::class);
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Authentication Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Protected Routes
+Route::middleware(['auth'])->group(function () {
+Route::resource('products', ProductController::class);
 });
